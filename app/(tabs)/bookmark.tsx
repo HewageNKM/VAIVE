2b1e74b07965ddf375059d14432d7911461e7a67
Nadun Kawishika <107237482+HewageNKM@users.noreply.github.com>
2024-07-10 20:14:21 +0530

    Bookmark UI complete(Logic to be added)

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import {View,Text} from "react-native";
+import {FlatList, Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import SearchInput from "@/components/SearchInput";
+import EmptyState from "@/components/EmptyState";
+import VideoCard from "@/components/VideoCard";
 
 const Bookmark = () => {
     return (
-        <View>
-            <Text>Bookmark</Text>
-        </View>
+        <SafeAreaView className="h-full bg-primary">
+            <FlatList data={[]} keyExtractor={() => {
+            }} ListHeaderComponent={() => (
+                <View className='my-6 px-4'>
+                    <Text className="font-pmedium text-xl text-gray-100">
+                        Bookmark
+                    </Text>
+                    <View className="mt-6 mb-8">
+                        <SearchInput initialQuery={''}/>
+                    </View>
+                </View>
+            )} renderItem={({item}) => (<VideoCard video={item} key={item.$id}/>)} ListEmptyComponent={() => (
+                <EmptyState title="No Videos Found" subTitle={`Add to Bookmark`}/>
+            )}>
+            </FlatList>
+        </SafeAreaView>
     );
 };
 
