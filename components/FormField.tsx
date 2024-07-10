8560159c2f4c973c6ab4d59dd423bb31e0028d4e
Nadun Kawishika <107237482+HewageNKM@users.noreply.github.com>
2024-07-10 20:16:59 +0530

    FormField password eye position improved

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -11,7 +11,7 @@ const FormField = ({title,value,placeholder,handleChangeText,otherStyles}) => {
             <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
             <View className="w-full flex-row h-16 bg-black-100 border-2 border-black-200 rounded-2xl focus:border-secondary items-center">
                 <TextInput secureTextEntry={title === "Password" && !showPassword} value={value} onChangeText={handleChangeText} placeholderTextColor="#7b7b8b" placeholder={placeholder} className="flex-1 p-2 text-white font-psemibold text-base"/>
-                {title === "Password" && (<TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                {title === "Password" && (<TouchableOpacity className="mr-2" onPress={() => setShowPassword(!showPassword)}>
                     <Image resizeMode="contain" className="w-6 h-6" source={showPassword ? icons.eye : icons.eyeHide}/>
                 </TouchableOpacity>)}
             </View>
